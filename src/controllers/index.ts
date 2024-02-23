247be4e61d31ba278ccb61cd336e0f2be3900552
Marian1017l <mariana.lopez35806@ucaldas.edu.co>
2024-02-23 16:16:24 -0500

    Creacion de los controladores para las entidades

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -4,3 +4,7 @@ export * from './usuario-login.controller';
 export * from './rol-menu.controller';
 export * from './menu-rol.controller';
 export * from './usuario-rol.controller';
+export * from './usuario.controller';
+export * from './menu.controller';
+export * from './login.controller';
+export * from './rol.controller';
